fbc08522551b302c36f8cb654edaf6f80f0ac809
Armando <gutierrezmartinezjosearmando@gmail.com>
2021-04-14 16:42:02 -0500

    Primer commit

---
+++ webpack.prod.js
@@ -0,0 +1,82 @@
+const HtmlWebPackPlugin    = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require ('optimize-css-assets-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    optimization:{
+        minimizer:[new OptimizeCssAssetsWebpackPlugin () ]
+    },
+    output: {
+        filename:'main.[contentHash].js'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ] 
+            },
+            {
+                test:/\.css$/,
+                exclude:/styles\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/styles\.css$/,
+                use:[
+                    miniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    minimize: false
+                },
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            // esModule: false,
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+
+        new miniCssExtractPlugin({
+            filename: '[name].[contentHash].css',
+            ignoreOrder: false
+        }),
+        new MinifyPlugin(),
+        new CleanWebpackPlugin(),
+
+        new CopyPlugin({
+            patterns:[
+                { from: 'src/assets', to: 'assets/' }
+            ]
+        })
+    ]
+
+}
+
